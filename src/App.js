2e9f97fb0d227015673ba4673e90679966d8492e
deepak <deepak@g.com>
2022-08-29 22:35:28 +0530

    secon commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         React Js with Deepak
+         React Js with Deepak Gupta
         </p>
         <a
           className="App-link"
